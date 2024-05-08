194153b48381a3d68eea05439d8b9ce27864d1cf
HenryKim2022 <autromatron@gmail.com>
2024-05-08 14:29:08 +0700

    opmitized merged-config.js

--- public/plugins/leaflet-official/leaflet-map-merged-config.js
+++ public/plugins/leaflet-official/leaflet-map-merged-config.js
@@ -68,6 +68,7 @@ function populateMapWithMarkers(map, markersLayer) {
             const marker = L.marker(coordinates, tooltipData);
 
             const institu_name = f.properties['institu_name'] || "none";
+            const institu_npsn = f.properties['institu_npsn'] || "none";
             const imgLogo = f.properties['institu_logo'] || "none";
             const institu_addr = f.properties['institu_address'] || "none";
             const institu_images = f.properties['institu_image'] || "none";
@@ -83,6 +84,7 @@ function populateMapWithMarkers(map, markersLayer) {
                     <div class='d-flex flex-column p-0'>
                     <span style='padding-bottom:2px;'><strong>Coordinates: </strong>${coordinates}<br></span>
                     <span style='padding-bottom:2px;'><strong>Name: </strong>${institu_name}<br></span>
+                    <span style='padding-bottom:2px;'><strong>NPSN: </strong>${institu_npsn}<br></span>
                     <span style='padding-bottom:2px;'><strong>Logo: </strong>
                         <img src='${isImgLogoExist}' alt='${institu_name} Logo' width='${imgWidth}'><br>
                     </span>
@@ -105,47 +107,84 @@ function populateMapWithMarkers(map, markersLayer) {
     addSearchControl(map, markersLayer, 'tobesearch');
 }
 
+
 function addMarkerOnContextMenu(map, markersLayer) {
     map.on('contextmenu taphold', function (e) {
         var LAT = e.latlng.lat.toFixed(7);
         var LNG = e.latlng.lng.toFixed(7);
+        const institu_name = "New Marker";
+        const institu_npsn = "Fill data!";
+        const imgLogo = imgu; // Corrected variable name
+        const institu_addr = "Fill data!";
+        const institu_images = imgu;
+        const last_update = "never";
 
-        // const institu_name = "New Marker";
-        // const imgLogo = imgu; // Corrected variable name
-        // const institu_addr = "Fill data!";
-        // const institu_images = imgu;
-        // const last_update = "never";
+        const tooltipData = {
+            tobesearch: institu_name
+        };
+        const marker = L.marker(new L.latLng([LAT, LNG]), tooltipData);
 
-        // const tooltipData = {
-        //     tobesearch: institu_name
-        // };
-        // const marker = L.marker(new L.latLng([LAT, LNG]), tooltipData);
+        const fromRightClick_PopupContent = `
+            <div class='d-flex flex-column p-0'>
+                <span style='padding-bottom:2px;'><strong>Coordinates: </strong>${LAT}, ${LNG}<br></span>
+                <span style='padding-bottom:2px;'><strong>Name: </strong>${institu_name}<br></span>
+                <span style='padding-bottom:2px;'><strong>NPSN: </strong>${institu_npsn}<br></span>
+                <span style='padding-bottom:2px;'><strong>Logo: </strong>
+                    <img src='${imgLogo}' alt='${institu_name} Logo' width='30'><br>
+                </span>
+                <span style='padding-bottom:2px;'><strong>Address:</strong>${institu_addr}<br></span>
+                <span class='d-flex flex-column align-top text-start' style='padding-bottom:2px;'><strong>Images:</strong>
+                    <img src='${institu_images}' alt='${institu_name} Logo' width='80'><br>
+                </span>
+                <span style='padding-bottom:2px;'><strong>Last Update: </strong>${last_update}<br></span>
+                <div class='d-flex flex-col justify-content-between'>
+                    <button class="mark-cancel-btn mdi mdi-cancel">Cancel</button>
+                    <button class="mark-remove-btn mdi mdi-delete">Remove</button>
+                    <button class="mark-edit-btn mdi mdi-content-edit">Edit</button>
+                </div>
+            </div>
+        `;
 
-        // const fromRightClick_PopupContent = `
-        //     <div class='d-flex flex-column p-0'>
-        //         <span style='padding-bottom:2px;'><strong>Coordinates: </strong>${LAT}, ${LNG}<br></span>
-        //         <span style='padding-bottom:2px;'><strong>Name: </strong>${institu_name}<br></span>
-        //         <span style='padding-bottom:2px;'><strong>Logo: </strong>
-        //             <img src='${imgLogo}' alt='${institu_name} Logo' width='30'><br>
-        //         </span>
-        //         <span style='padding-bottom:2px;'><strong>Address:</strong>${institu_addr}<br></span>
-        //         <span style='padding-bottom:2px;'><strong>Images:</strong>
-        //             <img src='${institu_images}' alt='${institu_name} Logo' width='80'><br>
-        //         </span>
-        //         <span style='padding-bottom:2px;'><strong>Last Update: </strong>${last_update}<br></span>
-        //         <div class='d-flex flex-col justify-content-between'>
-        //             <button class="mark-cancel-btn mdi mdi-cancel">Cancel</button>
-        //             <button class="mark-save-btn mdi mdi-content-save">Save</button>
-        //         </div>
-        //     </div>
-        // `;
+        marker.bindTooltip(institu_name + "  ➟  " + institu_addr);
+        marker.bindPopup(fromRightClick_PopupContent);
+        marker.options.popupText = marker._popup._content;
+        markersLayer.addLayer(marker);
 
-        // marker.bindTooltip(institu_name + "  ➟  " + institu_addr);
-        // marker.bindPopup(fromRightClick_PopupContent);
-        // marker.options.popupText = marker._popup._content;
-        // markersLayer.addLayer(marker);
 
-        // // Add event listener to the parent container of the popup content
+        // Handle the "Edit" button click event within the marker's popup
+        marker.on('popupopen', function () {
+            var editButton = document.querySelector('.mark-edit-btn');
+            editButton.addEventListener('click', function () {
+                $('#editMarkModal').modal('show');
+                var saveButton = document.querySelector('.modal-save-btn');
+                if (saveButton) {
+                    saveButton.addEventListener('click', function () {
+                        $('#editMarkModal').modal('hide');
+                        marker.setPopupContent(updatedPopupContent);
+                    });
+                }
+                var cancelButton = document.querySelector('.mark-cancel-btn');
+                if (cancelButton) {
+                    cancelButton.addEventListener('click', function () {
+                        marker.closePopup();
+                    });
+                }
+            });
+            var cancelButton = document.querySelector('.mark-cancel-btn');
+            cancelButton.addEventListener('click', function () {
+                if (cancelButton) {
+                    marker.closePopup();
+                }
+            });
+            var deleteButton = document.querySelector('.mark-remove-btn');
+            deleteButton.addEventListener('click', function () {
+                if (deleteButton) {
+                    markersLayer.removeLayer(marker); // Remove the marker from the layer
+                }
+            });
+        });
+
+        // Add event listener to the parent container of the popup content
         // marker.on('popupopen', function () {
         //     const popupContent = marker.getPopup().getContent();
         //     const cancelBtn = popupContent.getElementsByClassName('mark-cancel-btn')[0];
@@ -154,8 +193,11 @@ function addMarkerOnContextMenu(map, markersLayer) {
         //         markersLayer.removeLayer(marker);
         //     });
         // });
+
+
     });
 }
+
 
 
 function getAddressFromCoordinates(coordinates, callback) {
