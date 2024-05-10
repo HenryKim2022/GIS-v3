f4007523ea8eaaeb2adee1d8e6960365568c6532
HenryKim2022 <autromatron@gmail.com>
2024-05-10 22:52:19 +0700

    disabling routing with mark

--- resources/views/css/v_cssheader_collections.blade.php
+++ resources/views/css/v_cssheader_collections.blade.php
@@ -135,6 +135,3 @@ href="{{ asset('public/plugins/leaflet-official/leaflet.routing.machine/dist/lea
 
 
 {{-- LeafletControlWindow (addons) --}}
-<link rel="stylesheet"
-{{-- href="{{ asset('public/plugins/leaflet-official/leaflet-control-window/src/L.Control.Window.css') }}" /> --}}
-{{-- <script src="https://unpkg.com/leaflet-control-window"></script> --}}
